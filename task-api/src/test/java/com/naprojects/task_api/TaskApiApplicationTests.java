4970d88e9511e87fdb5223d1859b9d8227f0afd1
Nipun Avishka De Zoysa <nipunavishka123@gmail.com>
2025-08-12 22:09:41 +0530

    Initial commit

---
+++ task-api/src/test/java/com/naprojects/task_api/TaskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naprojects.task_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
